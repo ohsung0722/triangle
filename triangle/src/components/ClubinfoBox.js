9fb35809f9439896ff03c181248f2eaad26f46ff
yoon764 <yoonji22704@gmail.com>
2025-05-18 01:46:58 +0900

    Feat : 동아리 개별 페이지 정보 박스 구현

---
+++ triangle/src/components/ClubinfoBox.js
@@ -0,0 +1,27 @@
+import React from "react";
+import '../components/ClubinfoBox.css';
+
+const Clubinfo_Box = () =>{
+    return(
+        <div className="ClubinfoBox_Container">
+                <table className="InfoBox_Table">
+                    <tr><td className="Info_Label">회장</td>
+                    <td className="Info_Value">회장정보</td>
+
+                    <td className="Info_Label">연락처</td>
+                    <td className="Info_Value">연락처정보</td></tr>
+
+                    <tr><td className="Info_Label">동아리방</td>
+                    <td className="Info_Value">동아리방정보</td>
+
+                    <td className="Info_Label">모집기간</td>
+                    <td className="Info_Value">모집기간정보</td></tr>
+
+                    <tr><td className="Info_Label">정기모임</td>
+                    <td className="Info_Value">정기모임정보</td></tr>
+                </table>
+        </div>
+    );
+}
+
+export default Clubinfo_Box;
