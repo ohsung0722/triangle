bb058d66199a50063534d3b5cd00730e5430ff8d
yoon764 <yoonji22704@gmail.com>
2025-05-31 01:30:47 +0900

    Refactor : 상세페이지 동아리분류별 색 변경

--- triangle/src/components/clubDetailTitle/clubDetailTitle.js
+++ triangle/src/components/clubDetailTitle/clubDetailTitle.js
@@ -7,6 +7,19 @@ const ClubDetailTitle = ( { club } ) => {
 
     const { name, category, area } = club;
 
+    const TitleGetCategoryColor = (category) => {   
+    switch (category) {
+      case '공연분과': return "#f06292";
+      case '체육분과': return "#ED1C24";
+      case '봉사분과': return "#81c784";
+      case '문화분과': return "#ba68c8";
+      case '학술분과': return "#64b5f6";
+      case '종교분과': return "#ffb74d";
+      default:
+        return "#000";
+    }
+  };
+
     return(
         <div className="clubDetailTitle_Container">
             <img
@@ -17,7 +30,7 @@ const ClubDetailTitle = ( { club } ) => {
             <div className="clubDetailTitle_clubTextContainer">
                 <div className="clubDetailTitle_clubTitle">{name}</div>
                 <div className="clubDetailTitle_subtitle">
-                    <span>{category}</span> | <span>{area}</span>
+                    <span style={ {color : TitleGetCategoryColor(category) } }>{category}</span> | <span>{area}</span>
                 </div>
             </div>
         </div>
